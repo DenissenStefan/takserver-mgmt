a64803047e56e6f8ab0d2ef1b0cbf27b564f794a
DenissenStefan <sw.denissen@gmail.com>
2022-11-14 19:22:11 +0100

    Added CSV parsing to the BulkUsers script

--- BulkUsers.sh
+++ BulkUsers.sh
@@ -9,10 +9,10 @@ XML_FILE="/opt/tak/CoreConfig.xml"
 # Variables below are automatically pulled from the config:
 IP=`hostname -I`
 HostName=$HOSTNAME
+
 #This is to remove the trailing space after the variable
 IP=`echo $IP | sed -e 's/^[[:space:]]*//'`
 
-# This needs to be pulled from coreconfig.xml in the future, needs to be an if/else statement. If intermediate CA then certificate X, else certificate X from coreconfig.xml
 # This outputs the CoreConfig node:
 CaPass=$(xmllint --xpath "//*[local-name()='Configuration']/*[local-name()='security']/*[local-name()='tls']/@keystorePass" $XML_FILE)
 ClientPass=$(xmllint --xpath "//*[local-name()='Configuration']/*[local-name()='security']/*[local-name()='tls']/@truststorePass" $XML_FILE)
@@ -26,11 +26,17 @@ ServerCert=${ServerCert#*'"'}; ServerCert=${ServerCert%'"'*}
 intermediateCert=${intermediateCert#*'"'}; intermediateCert=${intermediateCert%'"'*}
 
 if
+
 	[ -z "$intermediateCert" ]
+
 then
+
 	TrustStoreCert=$ServerCert
+
 else
+
 	TrustStoreCert=$intermediateCert
+
 fi
 
 # This removes the file extension and the path from the string:
@@ -38,93 +44,113 @@ TrustStoreCert=$(echo $TrustStoreCert | cut -c13- | rev | cut -c5- | rev)
 
 ######################################################################
 
-# This needs to come from a CSV in the future:
-UserGroup="ExampleGroup"
-
-File="users.txt"
-Users=$(cat $File)
-
-# Create a directory for storage of the user certificates
+# Create a directory for storage of the user certificates & data-packages
 mkdir -p "/opt/takdata/user-certificates"
-# Create a directory for storage of the user data-packages
 mkdir -p "/opt/takdata/data-packages"
 
-for User in $Users
+# Read the header entries from the CSV file
+while IFS="," read -r Username Group1 Group2
+
 do
-	# User Certificates
-	echo "Creating certificate for $User"
-	./makeCert.sh client $User
-	
-	# Without this wait functions certificate generation won't finish and the certificates won't be usable.
-	wait
 
-	# User account
-	echo "Creating user account for $User"
-	java -jar /opt/tak/utils/UserManager.jar certmod -g $UserGroup /opt/tak/certs/files/$User.pem
+	if
+		# Without this the script will skip the last line.
+    	[[ $Username != "" ]] ;
 
-	# The same as the wait function before, this makes sure the process will finish before the script continues.
-	wait
-	
-	# server.pref
+	then
+		
+		# User Certificates
+		echo "Creating certificate for $Username"
+		./makeCert.sh client $Username
 
-	echo "Creating server.pref file for $User"
+		# Without this wait functions certificate generation won't finish and the certificates won't be usable.
+		wait
+		
+		if
+			
+			# This checks if CSV entry of group 2 is empty
+			[ -z "$Group2" ] ;
 
-	echo "<?xml version='1.0' encoding='ASCII' standalone='yes'?>" > server.pref
-	echo "<preferences>" >> server.pref
-	echo "  <preference version=\"1\" name=\"cot_streams\">" >> server.pref
-	echo "    <entry key=\"count\" class=\"class java.lang.Integer\">1</entry>" >> server.pref
-	echo "    <entry key=\"description0\" class=\"class java.lang.String\">$HostName</entry>" >> server.pref
-	echo "    <entry key=\"enabled0\" class=\"class java.lang.Boolean\">true</entry>" >> server.pref
-	echo "    <entry key=\"connectString0\" class=\"class java.lang.String\">$IP:8089:ssl</entry>" >> server.pref
-	echo "  </preference>" >> server.pref
-	echo "  <preference version=\"1\" name=\"com.atakmap.app_preferences\">" >> server.pref
-	echo "    <entry key=\"displayServerConnectionWidget\" class=\"class java.lang.Boolean\">true</entry>" >> server.pref
-	echo "    <entry key=\"caLocation\" class=\"class java.lang.String\">$TrustStoreCert.p12</entry>" >> server.pref
-	echo "    <entry key=\"caPassword\" class=\"class java.lang.String\">$CaPass</entry>" >> server.pref
-	echo "    <entry key=\"clientPassword\" class=\"class java.lang.String\">$ClientPass</entry>" >> server.pref
-	echo "    <entry key=\"certificateLocation\" class=\"class java.lang.String\">$User.p12</entry>" >> server.pref
-	echo "  </preference>" >> server.pref
-	echo "</preferences>" >> server.pref
+		then
 
+			# creating a user account with a single group
+			echo "Creating user account for $Username"
+			java -jar /opt/tak/utils/UserManager.jar certmod -g $Group1 /opt/tak/certs/files/$Username.pem
 
-	# manifest.xml
+			# The same as the wait function before, this makes sure the process will finish before the script continues.
+			wait
 
-	echo "Creating manifest.xml file for $User"
+		else
 
-	echo "<MissionPackageManifest version=\"2\">" > manifest.xml
-	echo "  <Configuration>" >> manifest.xml
-	echo "    <Parameter name=\"uid\" value=\"af6f9606-4e37-4d85-b14e-3d0a99705a9d\"/>" >> manifest.xml
-	echo "    <Parameter name=\"name\" value=\"$HostName\"/>" >> manifest.xml
-	echo "    <Parameter name=\"onReceiveDelete\" value=\"false\"/>" >> manifest.xml
-	echo "  </Configuration>" >> manifest.xml
-	echo "  <Contents>" >> manifest.xml
-	echo "    <Content zipEntry=\"server.pref\" ignore=\"false\" />" >> manifest.xml
-	echo "    <Content zipEntry=\"$TrustStoreCert.p12\" ignore=\"false\" />" >> manifest.xml
-	echo "    <Content zipEntry=\"$User.p12\" ignore=\"false\" />" >> manifest.xml
-	echo "  </Contents>" >> manifest.xml
-	echo "</MissionPackageManifest>" >> manifest.xml
+			# creating a user account with two groups
+			echo "Creating user account for $Username"
+			java -jar /opt/tak/utils/UserManager.jar certmod -g $Group1 -g $Group2 /opt/tak/certs/files/$Username.pem
 
-	echo "zipping files for $User"
+			# The same as the wait function before, this makes sure the process will finish before the script continues.
+			wait
 
-	# Zip files into a datapackage
-	zip -j "/opt/takdata/data-packages/dp-$User-$HostName.zip" "/opt/tak/certs/manifest.xml" "/opt/tak/certs/server.pref" "/opt/tak/certs/files/$TrustStoreCert.p12" "/opt/tak/certs/files/$User.p12"
+		fi
 
-	wait
+		# server.pref
 
-	# Create a subdirectory for the user certificates
-	mkdir -p "/opt/takdata/user-certificates/$User"
+		echo "Creating server.pref file for $Username"
 
-	# This will move all files for the user to the user-certificates folder created above
-	find . -name "$User*.*" -exec mv '{}' "/opt/takdata/user-certificates/$User/" \;
-
-	echo "-------------------------------------------------------------"
-	echo "Created certificate data package for $User @ $HostName as takdata/data-packages/dp-$User-$HostName.zip"
+		echo "<?xml version='1.0' encoding='ASCII' standalone='yes'?>" > server.pref
+		echo "<preferences>" >> server.pref
+		echo "  <preference version=\"1\" name=\"cot_streams\">" >> server.pref
+		echo "    <entry key=\"count\" class=\"class java.lang.Integer\">1</entry>" >> server.pref
+		echo "    <entry key=\"description0\" class=\"class java.lang.String\">$HostName</entry>" >> server.pref
+		echo "    <entry key=\"enabled0\" class=\"class java.lang.Boolean\">true</entry>" >> server.pref
+		echo "    <entry key=\"connectString0\" class=\"class java.lang.String\">$IP:8089:ssl</entry>" >> server.pref
+		echo "  </preference>" >> server.pref
+		echo "  <preference version=\"1\" name=\"com.atakmap.app_preferences\">" >> server.pref
+		echo "    <entry key=\"displayServerConnectionWidget\" class=\"class java.lang.Boolean\">true</entry>" >> server.pref
+		echo "    <entry key=\"caLocation\" class=\"class java.lang.String\">$TrustStoreCert.p12</entry>" >> server.pref
+		echo "    <entry key=\"caPassword\" class=\"class java.lang.String\">$CaPass</entry>" >> server.pref
+		echo "    <entry key=\"clientPassword\" class=\"class java.lang.String\">$ClientPass</entry>" >> server.pref
+		echo "    <entry key=\"certificateLocation\" class=\"class java.lang.String\">$Username.p12</entry>" >> server.pref
+		echo "  </preference>" >> server.pref
+		echo "</preferences>" >> server.pref
 
 
-done
+		# manifest.xml
+
+		echo "Creating manifest.xml file for $Username"
+
+		echo "<MissionPackageManifest version=\"2\">" > manifest.xml
+		echo "  <Configuration>" >> manifest.xml
+		echo "    <Parameter name=\"uid\" value=\"af6f9606-4e37-4d85-b14e-3d0a99705a9d\"/>" >> manifest.xml
+		echo "    <Parameter name=\"name\" value=\"$HostName\"/>" >> manifest.xml
+		echo "    <Parameter name=\"onReceiveDelete\" value=\"false\"/>" >> manifest.xml
+		echo "  </Configuration>" >> manifest.xml
+		echo "  <Contents>" >> manifest.xml
+		echo "    <Content zipEntry=\"server.pref\" ignore=\"false\" />" >> manifest.xml
+		echo "    <Content zipEntry=\"$TrustStoreCert.p12\" ignore=\"false\" />" >> manifest.xml
+		echo "    <Content zipEntry=\"$Username.p12\" ignore=\"false\" />" >> manifest.xml
+		echo "  </Contents>" >> manifest.xml
+		echo "</MissionPackageManifest>" >> manifest.xml
+
+		echo "zipping files for $Username"
+
+		# Zip files into a datapackage
+		zip -j "/opt/takdata/data-packages/dp-$Username-$HostName.zip" "/opt/tak/certs/manifest.xml" "/opt/tak/certs/server.pref" "/opt/tak/certs/files/$TrustStoreCert.p12" "/opt/tak/certs/files/$Username.p12"
+
+		wait
+
+		# Create a subdirectory for the user certificates
+		mkdir -p "/opt/takdata/user-certificates/$Username"
+
+		# This will move all files for the user to the user-certificates folder created above
+		find . -name "$Username*.*" -exec mv '{}' "/opt/takdata/user-certificates/$Username/" \;
+
+		echo "-------------------------------------------------------------"
+		echo "Created certificate data package for $Username @ $HostName as takdata/data-packages/dp-$Username-$HostName.zip"
+
+	fi
+
+# This makes sure the script starts from the second line of the CSV file.
+done < <(tail -n +2 users.csv)
 
 # Cleanup of server.pref & manifest.xml
 rm "/opt/tak/certs/manifest.xml"
 rm "/opt/tak/certs/server.pref"
-
-
--- README.md
+++ README.md
@@ -3,24 +3,24 @@
 This script is meant to create data packages on the TAKserver for initial configuration of connected devices.
 
 # Usage
-Copy the script to the `/opt/tak/certs` folder on the takserver (ensure you have execute permissions on the script)
-Run the script by entering `.\bulk-client-certificates.sh` as the tak user (`sudo su tak`)
+Copy the script to the `/opt/tak/certs` folder on the takserver (ensure you have execute permissions on the script). Edit the `users.csv` per example with at the end a blank line.
+Run the script by entering `.\BulkUsers.sh` in the terminal.
 
 # Changelog:
 
-05-10-2022: Built first script to generate bulk user certificates
-21-10-2022: Added parts of [Cloud-RF datapackage script](https://github.com/Cloud-RF/tak-server/blob/main/scripts/certDP.sh) to bulk generate data packages with connection details for the TAK server. Also added a part for user generation.
-22-10-2022: Added a feature where the user is automatticaly added to a group instead of the default `__ANON__` group.
-23-10-2022: Added Hostname variable for the takserver name and pull IP Address from hostname
-27-10-2022: Created separate folders for user certificates en data packages.
-28-10-2022: Changed the user creation from flat file to certificate based
-08-11-2022: Changed the user-certificate and datapackage folder locations.
-13-11-2022: Added XML parsing from the CoreConfig file.
+| Date | Change  |
+| --- | --- |
+| 05-10-2022 | Built first script to generate bulk user certificates |
+| 21-10-2022 | Added parts of [Cloud-RF datapackage script](https://github.com/Cloud-RF/tak-server/blob/main/scripts/certDP.sh) to bulk generate data packages with connection details for the TAK server. Also added a part for user generation. |
+| 22-10-2022 | Added a feature where the user is automatticaly added to a group instead of the default `__ANON__` group. |
+| 23-10-2022 | Added Hostname variable for the takserver name and pull IP Address from hostname |
+| 27-10-2022 | Created separate folders for user certificates en data packages. |
+| 28-10-2022 | Changed the user creation from flat file to certificate based |
+| 08-11-2022 | Changed the user-certificate and datapackage folder locations. |
+| 13-11-2022 | Added XML parsing from the CoreConfig file. |
+| 14-11-2022 | Added CSV parsing from users.csv |
 
 # Future features
-- Parse CSV for Username, group(s) & EUD
-- Build option for multiple groups
-- Build option for difference in ATAK/WINTAK
 - Add TAKchat plugin to data package
 - Configure TAKchat plugin
 - Separate package for certificate enrollment
